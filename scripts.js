188247087a3c60f18f621105131df0976640efc2
Ruben Medina Martinez <ruskymartinez@gmail.com>
2022-08-13 22:09:25 +0200

    Finalización ejercicio 11 JavaScript

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log("Bucle con el FOR")
+for(let i = 0; i < 10; i++){
+    console.log("I ❤ code")
+}
+console.log("Bucle con el WHILE")
+let b = 0;
+let num2 = 0;
+while (b < 10) {
+    num2++
+    console.log("I ❤ code"+" "+num2);
+    b++;
+}
